b0e1f4ae9a947195764c19fd7ead9ea36867ff95
dev-frog <kali.nisga@gmail.com>
2020-08-13 00:22:50 +0600

    mega code

--- sensor.ino
+++ sensor.ino
@@ -1,17 +1,38 @@
-
-#include <LiquidCrystal.h>
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-void setup() {
-  lcd.begin(16, 2);
+#include <OzIDManager.h>
+#include <OzLcdDisplayController.h>
+ 
+// global pointers
+OzIDManager* manager;
+OzLcdController* lcdController;
+ 
+const int backlight_pin = 3;
+const int RS = 6;
+const int RW = 7;
+const int EN = 8;
+const int D4 = 9;
+const int D5 = 10;
+const int D6 = 11;
+const int D7 = 12;
+ 
+void setup()
+{
+  Serial.begin(115200);
+ 
+  manager = new OzIDManager;
+  manager->_sendACK = true;
+  manager->_checksum = true;
+   
+  OzCommunication::setIDManager(manager);
+  lcdController = new OzLcdController(RS, RW, EN, D4, D5, D6, D7);
+  lcdController->SetBacklight(backlight_pin, 100); //100% backlight
+ 
+  int x=1;
+  manager->sendLinkSetup();
+  manager->PrintWelcomeLine(lcdController, x++, "MyLCD");
+  lcdController->LoginDisplaySize(16, 2);
 }
-void loop() {
-
-  lcd.setCursor(2, 0);
-
-  lcd.print("Hello World!");
-
-  lcd.setCursor(2, 1);
-
-  lcd.print("LCD tutorial");
+ 
+void loop()
+{
+  OzCommunication::communicate();
 }
