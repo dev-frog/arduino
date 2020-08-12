af6b1bcb86027bcc359c7797941a6ed5ead733da
dev-frog <kali.nisga@gmail.com>
2020-08-13 01:01:43 +0600

    test 4

--- sensor.ino
+++ sensor.ino
@@ -1,26 +1,14 @@
 #include <LiquidCrystal.h>
-const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+const int rs = 8, en = 9, d4 = 10, d5 = 11, d6 = 12, d7 = 13;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+
 void setup() {
   lcd.begin(16,2);
-  lcd.setCursor(0,0);
-  lcd.print("    COUNTER");
-  delay(100);
-
-  int a=0;
-  lcd.setCursor(0,1);
-  lcd.print("       ");
-  lcd.print(a);
-    while(a<=100){
-      a=a+1;
-      delay(1000);
-      lcd.setCursor(0,1);
-      lcd.print("       ");
-      lcd.print(a);
-    }
+  lcd.print(“Hello !”);
 }
 
-
-void loop(){
-  lcd.clear();
+void loop() {
+  lcd.setCursor(0, 1);
+  lcd.print(millis()/1000);
 }
