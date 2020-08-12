e4fe1048e4665a2953e88d17f87ee0f7181edb62
dev-frog <kali.nisga@gmail.com>
2020-08-13 00:46:46 +0600

    test 2

--- sensor.ino
+++ sensor.ino
@@ -1,23 +1,26 @@
 #include <LiquidCrystal.h>
 const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-// LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 void setup() {
-  pinMode(rs,OUTPUT);
-  pinMode(en,OUTPUT);
-  pinMode(d4,OUTPUT);
-  pinMode(d5,OUTPUT);
-  pinMode(d6,OUTPUT);
-  pinMode(d7,OUTPUT);
-  
-}
-void loop() {
-  digitalWrite(rs,HIGH);
-  digitalWrite(en,HIGH);
-  digitalWrite(d4,HIGH);
-  digitalWrite(d5,HIGH);
-  digitalWrite(d6,HIGH);
-  digitalWrite(d7,HIGH);
+  lcd.begin(16,2);
+  lcd.setCursor(0,0);
+  lcd.print("    COUNTER");
+  delay(100);
 
-  delay(1000);
-  
+  int a=0;
+  lcd.setCursor(0,1);
+  lcd.print("       ");
+  lcd.print(a);
+    while(a<=100){
+      a=a+1;
+      delay(1000);
+      lcd.setCursor(0,1);
+      lcd.print("       ");
+      lcd.print(a);
+    }
+}
+
+
+void loop(){
+  lcd.clear();
 }
