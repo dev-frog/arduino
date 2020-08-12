e6e37da6ee355a3bf96c61ab6d6a1d5e2e84f3a8
dev-frog <kali.nisga@gmail.com>
2020-08-13 00:47:36 +0600

    test 3

--- sensor.ino
+++ sensor.ino
@@ -1,5 +1,5 @@
 #include <LiquidCrystal.h>
-const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 void setup() {
   lcd.begin(16,2);
